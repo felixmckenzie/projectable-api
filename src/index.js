a7b65391686b233afdee81680f3158b2e75769c8
Robert Neville <robertneville27@gmail.com>
2022-10-27 10:57:34 +1000

    refactor server code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-import { startServer, PORT, HOST } from './server.js';
-startServer(PORT, HOST);
+import app, { PORT, HOST } from './server.js';
+import logger from './logger.js';
+
+app.listen(PORT, HOST, (error) => {
+  if (error) {
+    return logger.error(error.message);
+  }
+
+  logger.info(`Server listening on port ${PORT}`);
+});
--- src/server.js
+++ src/server.js
@@ -1,28 +1,19 @@
 import express from 'express';
-import logger from './logger.js';
 import routes from './routes/index.js';
 
-export function startServer(PORT, HOST) {
-  const app = express();
+const app = express();
 
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-  // Projects router:
-  app.use('/projects', routes.projectsRouter);
-  // Tasks router:
-  app.use('/tasks', routes.tasksRouter);
-  // Comments router
-  app.use('/comments', routes.commentsRouter)
-
-  app.listen(PORT, HOST, (error) => {
-    if (error) {
-      return logger.error(error.message);
-    }
-
-    logger.info(`Server listening on port ${PORT}`);
-  });
-}
+// Projects router:
+app.use('/projects', routes.projectsRouter);
+// Tasks router:
+app.use('/tasks', routes.tasksRouter);
+// Comments router
+app.use('/comments', routes.commentsRouter)
 
 export const PORT = process.env.PORT || 3000;
 export const HOST = '0.0.0.0';
+
+export default app;
