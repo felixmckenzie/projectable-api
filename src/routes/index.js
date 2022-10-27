63a89fdeb285dc48e92459b343d4f874f8bd5721
Robert Neville <robertneville27@gmail.com>
2022-10-27 10:44:36 +1000

    start setting up routes

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+import projectsRouter from "./projects.js";
+import tasksRouter from "./tasks.js";
+import commentsRouter from "./comments.js";
+
+export default {
+  projectsRouter,
+  tasksRouter,
+  commentsRouter,
+};
--- src/server.js
+++ src/server.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import logger from './logger.js';
+import routes from './routes/index.js';
 
 export function startServer(PORT, HOST) {
   const app = express();
 
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+
+  // Projects router:
+  app.use('/projects', routes.projectsRouter);
+  // Tasks router:
+  app.use('/tasks', routes.tasksRouter);
+  // Comments router
+  app.use('/comments', routes.commentsRouter)
 
   app.listen(PORT, HOST, (error) => {
     if (error) {
