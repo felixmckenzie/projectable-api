e23f94668bfabeaf9fee196d952e1e854195607a
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-01 13:40:29 +1100

    refactored auth functions

--- src/firebaseApp.js
+++
@@ -1,8 +0,0 @@
-import { initializeApp } from 'firebase/app';
-import { development, production } from './config/firebase.js';
-
-console.log(JSON.stringify(development));
-const config =
-  process.env.NODE_ENV === 'development' || 'test' ? development : production;
-
-export default initializeApp(config);
--- src/routes/usersHelpers.js
+++ src/routes/usersHelpers.js
@@ -1,32 +1,58 @@
+import admin from 'firebase-admin';
+import { initializeApp } from 'firebase';
+import { firebaseClientConfig } from '../config/firebaseClientKey';
 import {
   getAuth,
   updateProfile,
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
 } from 'firebase/auth';
-import firebaseApp from '../firebaseApp.js';
 
-const auth = getAuth(firebaseApp);
+// Initialize the Firebase Client SDK
+initializeApp(firebaseClientConfig);
 
-export async function registerUser(email, password) {
-  return createUserWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      const user = userCredential.user;
-      return user;
+export async function registerUser(userDetails) {
+  return admin
+    .auth()
+    .createUser({
+      email: userDetails.email,
+      emailVerified: true,
+      password: userDetails.password,
+      displayName: userDetails.displayName,
+      disabled: false,
+    })
+    .then(async (userRecord) => {
+      return userRecord;
     })
     .catch((error) => {
       return { error: error.message };
     });
 }
 
-export async function loginUser(email, password) {
-  return signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => {
-      return userCredential.user;
+export async function loginUser(userDetails) {
+  const clientAuth = getAuth();
+  const signInResult = signInWithEmailAndPassword(
+    clientAuth,
+    userDetails.email,
+    userDetails.password
+  )
+    .then(async (userCredential) => {
+      const userIdToken = await clientAuth.currentUser.getIdTokenResult(false);
+      console.log(`userIdToken obj is\n ${JSON.stringify(userIdToken)}`);
+      return {
+        idToken: userIdToken.token,
+        refreshToken: userCredential.user.refreshToken,
+        email: userCredential.user.email,
+        emailVerified: userCredential.user.emailVerified,
+        displayName: userCredential.user.displayName,
+        photoURL: userCredential.user.photoURL,
+        uid: userCredential.user.uid,
+      };
     })
     .catch((error) => {
       return { error: error.message };
     });
+    return signInResult
 }
 
 export async function initUserProfile(user, profileInfo) {
@@ -43,3 +69,5 @@ export async function initUserProfile(user, profileInfo) {
       };
     });
 }
+
+
