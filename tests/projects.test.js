7189cb95886a708be90305e91e9523320c535c54
Felix Mckenzie <felixmckenzie90@gmail.com>
2022-11-02 14:35:05 +1100

    refactored project tests

--- tests/projects.test.js
+++ tests/projects.test.js
@@ -1,10 +1,45 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import app from '../src/server.js';
+import { registerUser, loginUser } from '../src/routes/usersHelpers.js';
+import admin from 'firebase-admin';
 import { databaseConnector, databaseDisconnector } from '../src/database.js';
 
 const DATABASE_URI =
   process.env.DATABASE_URI || 'mongodb://localhost:27017/projectable-tests';
+
+const newUserDetails = {
+  email: 'john_test_user@mail.com',
+  password: 'johntest1234',
+  firstName: 'John',
+  lastName: 'Test',
+  username: 'johntester'
+
+};
+let userRecord;
+let loginOutcome;
+
+beforeAll(async () => {
+  try {
+    userRecord = await registerUser(newUserDetails);
+    loginOutcome = await loginUser(newUserDetails)
+    console.log(loginOutcome.idToken.token)
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+afterAll(async () => {
+await admin
+  .auth()
+  .deleteUser(userRecord.uid)
+  .then(() => {
+    console.log('Successfully deleted user');
+  })
+  .catch((error) => {
+    console.log('Error deleting user:', error);
+  });
+});
 
 // set up before-tests and after-tests operations
 beforeEach(async () => {
@@ -17,11 +52,16 @@ afterEach(async () => {
 
 describe('Projects', () => {
   it('creates a project', async () => {
-    const response = await request(app).post('/projects').send({
-      name: 'A new project',
-      description: 'This is a test project',
-      createdBy: 'John Smith',
-    });
+    const response = await request(app)
+      .post('/api/projects')
+      .set("Authorization", "Bearer " + loginOutcome.idToken.token)
+      .send({
+        name: 'A new project',
+        description: 'This is a test project',
+        createdBy: 'John Smith',
+      });
+     
+     
     expect(response.statusCode).toEqual(201);
     const project = response.body;
     expect(project.name).toEqual('A new project');
