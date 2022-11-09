5b6dc031a92b2b465b12d4232898ba1b1045cb1a
Robert Neville <robertneville27@gmail.com>
2022-11-09 15:57:33 +1000

    change dev port to 3001 (avoid conflicts with client dev server port)

--- src/server.js
+++ src/server.js
@@ -44,7 +44,7 @@ app.use('/api/tasks', routes.tasksRouter);
 // Comments router
 app.use('/api/comments', routes.commentsRouter);
 
-export const PORT = process.env.PORT || 3000;
+export const PORT = process.env.PORT || 3001;
 export const HOST = '0.0.0.0';
 
 export default app;
